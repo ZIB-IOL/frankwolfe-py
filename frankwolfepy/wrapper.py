98845b9d9ed4b46d657b0b46e46c91c222a7cc9a
Victor <victor.thouvenot@gmail.com>
2024-03-25 18:21:42 +0100

    change name wrapper function

--- frankwolfepy/wrapper.py
+++ frankwolfepy/wrapper.py
@@ -2,5 +2,6 @@ import juliacall
 jl = juliacall.newmodule("FW")
 jl.seval("using PythonCall")
 
-def wrap_obj_func(f):
-    return jl.seval("f -> x -> pyconvert(Float64, f(x))")(f)
+def wrap_objective_function(f):
+    return jl.seval("f -> (x -> pyconvert(Float64, f(x)))")(f)
+
